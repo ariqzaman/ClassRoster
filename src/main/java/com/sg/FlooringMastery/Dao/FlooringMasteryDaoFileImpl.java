8a3322696f7dbe51cce130fce551bf7c4251f6b2
Emerson Havener <emerson@nevada.unr.edu>
2023-12-13 12:19:10 -0800

    Implements method headers for ServiceLayerDaoFileImpl

--- src/main/java/com/sg/FlooringMastery/Dao/FlooringMasteryDaoFileImpl.java
+++ src/main/java/com/sg/FlooringMastery/Dao/FlooringMasteryDaoFileImpl.java
@@ -1,6 +1,27 @@
 package com.sg.FlooringMastery.Dao;
 
-public class FlooringMasteryDaoFileImpl {
+import com.sg.FlooringMastery.Dto.Order;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+public class FlooringMasteryDaoFileImpl implements FlooringMasteryDao {
+    private int autoIncrement = 1;
+    Map<Integer, Order> orders = new HashMap<>();
+
+    @Override
+    public Order addOrder(String OrderDate, String CustomerName, String State, String ProductType, BigDecimal Area) throws FlooringMasteryPersistenceException {
+        return null;
+    }
+
+    @Override
+    public Order editOrder(String OrderDate, int OrderNumber) throws FlooringMasteryPersistenceException {
+        return null;
+    }
+
+    @Override
+    public Order removeOrder(String OrderDate, int OrderNumber) throws FlooringMasteryPersistenceException {
+        return null;
+    }
 }
