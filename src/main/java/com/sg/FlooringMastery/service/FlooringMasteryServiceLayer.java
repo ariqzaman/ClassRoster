c4952e31350c6427e9bd45e79860eae6dccbf1e3
Emerson Havener <emerson@nevada.unr.edu>
2023-12-14 11:08:30 -0800

    Implements tests for service layer and fixes regex bug for customer name validation

--- src/main/java/com/sg/FlooringMastery/service/FlooringMasteryServiceLayer.java
+++ src/main/java/com/sg/FlooringMastery/service/FlooringMasteryServiceLayer.java
@@ -1,16 +1,14 @@
 package com.sg.FlooringMastery.service;
 
-import com.sg.FlooringMastery.Dao.FlooringMasteryDao;
 import com.sg.FlooringMastery.Dao.FlooringMasteryPersistenceException;
 import com.sg.FlooringMastery.Dto.Order;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public interface FlooringMasteryServiceLayer {
     public Order updateNewOrderWithCustomerName(Order order, String name) throws CustomerNameInvalidException;
-    public Order updateNewOrderWithState(Order order, String name) throws StateNotTaxbleException, FlooringMasteryPersistenceException;
+    public Order updateNewOrderWithState(Order order, String name) throws StateNotTaxableException, FlooringMasteryPersistenceException;
     public Order updateNewOrderWithProductType(Order order, String product) throws ProductTypeNotAvailableException, FlooringMasteryPersistenceException;
-    public Order updateNewOrderWithArea(Order order, BigDecimal area) throws MinimumOrderSizeNotMetException;
+    public Order updateNewOrderWithArea(Order order, BigDecimal area) throws MinimumOrderAreaNotMetException;
     public Order confirmOrder(Order order);
 }
--- src/main/java/com/sg/FlooringMastery/service/FlooringMasteryServiceLayerImpl.java
+++ src/main/java/com/sg/FlooringMastery/service/FlooringMasteryServiceLayerImpl.java
@@ -1,39 +1,49 @@
 package com.sg.FlooringMastery.service;
 
 import com.sg.FlooringMastery.Dao.FlooringMasteryDao;
+import com.sg.FlooringMastery.Dao.FlooringMasteryDaoFileImpl;
 import com.sg.FlooringMastery.Dao.FlooringMasteryPersistenceException;
 import com.sg.FlooringMastery.Dto.Order;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.regex.Pattern;
 
 public class FlooringMasteryServiceLayerImpl implements FlooringMasteryServiceLayer {
     FlooringMasteryDao dao;
 
+    FlooringMasteryServiceLayerImpl() {
+        this.dao = new FlooringMasteryDaoFileImpl();
+    }
+
     @Override
     public Order updateNewOrderWithCustomerName(Order order, String name) throws CustomerNameInvalidException {
-        if (Pattern.compile("^[a-zA-Z0-9.,]+$").matcher(name).matches()) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new CustomerNameInvalidException("Name cannot be blank.");
+        }
+
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9., ]+$");
+        boolean matches = pattern.matcher(name).matches();
+        if (matches) {
             order.setCustomerName(name);
             return order;
         } else {
-            throw new CustomerNameInvalidException();
+            throw new CustomerNameInvalidException("Name contains invalid characters.");
         }
     }
 
     @Override
-    public Order updateNewOrderWithState(Order order, String stateAbbreviation) throws StateNotTaxbleException, FlooringMasteryPersistenceException {
+    public Order updateNewOrderWithState(Order order, String stateAbbreviation) throws StateNotTaxableException, FlooringMasteryPersistenceException {
         if (!dao.getTaxes().stream().filter(tax -> stateAbbreviation.equals(tax.getStateAbbreviation())).toList().isEmpty()) {
             order.setState(stateAbbreviation);
             return order;
         } else {
-            throw new StateNotTaxbleException();
+            throw new StateNotTaxableException();
         }
     }
 
     @Override
     public Order updateNewOrderWithProductType(Order order, String productType) throws ProductTypeNotAvailableException, FlooringMasteryPersistenceException {
-        if (!dao.getProducts().stream().filter(product -> product.equals(product.getProductType())).toList().isEmpty()) {
+        if (!dao.getProducts().stream().filter(product -> productType.equals(product.getProductType())).toList().isEmpty()) {
             order.setProductType(productType);
             return order;
         } else {
@@ -42,12 +52,12 @@ public class FlooringMasteryServiceLayerImpl implements FlooringMasteryServiceLa
     }
 
     @Override
-    public Order updateNewOrderWithArea(Order order, BigDecimal area) throws MinimumOrderSizeNotMetException {
+    public Order updateNewOrderWithArea(Order order, BigDecimal area) throws MinimumOrderAreaNotMetException {
         if (new BigDecimal(100).compareTo(area) <= 0) {
             order.setArea(area);
             return order;
         } else {
-            throw new MinimumOrderSizeNotMetException();
+            throw new MinimumOrderAreaNotMetException();
         }
     }
 
